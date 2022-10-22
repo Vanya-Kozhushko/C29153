90be8f3fc2e0a14adf2c17c0e2b097d68b10696c
Vanya <vanya.love64@gmail.com>
2022-10-22 09:51:28 +0300

    init

---
+++ viktorina/main.py
@@ -0,0 +1,66 @@
+score = 0
+
+print('В каком году началась вторая мировая война?')
+print('а) 1914')
+print('б) 1939')
+print('в) 1937')
+print('г) 1941')
+
+answer = input('выбирите ответ: ')
+if answer == 'б':
+    print('правильно')
+    score = 1
+else:
+    print('Не правильно')
+
+print('Чем быстрее добратся до работы?')
+print('а) пешком')
+print('б) машиной')
+print('в) маршруткой')
+print('г) обходными путями через лес')
+
+answer = input('выбирите ответ: ')
+if answer == 'г':
+    print('правильно')
+    score = 1
+else:
+    print('Не правильно')
+
+print('Чем легче добыть камень?')
+print('а) киркой')
+print('б) собакой')
+print('в) рукой')
+print('г) mi band')
+
+answer = input('выбирите ответ: ')
+if answer == 'а':
+    print('правильно')
+    score = 1
+else:
+    print('Не правильно')
+
+print('Что такое фиксики?')
+print('а) болты')
+print('б) гайки')
+print('в) большой большой секрет')
+print('г) чшшшшшшшш')
+
+answer = input('выбирите ответ: ')
+if answer == 'в':
+    print('правильно')
+    score = 1
+else:
+    print('Не правильно')
+
+print('Что?')
+print('а) почему?')
+print('б) зачем?')
+print('в) как?')
+print('г) а ну ладно')
+
+answer = input('выбирите ответ: ')
+if answer == 'г':
+    print('правильно')
+    score = 1
+else:
+    print('Не правильно')
